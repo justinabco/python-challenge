ad3c68279c4b49465137f8f9787423d8f4a29544
Justin Co <justin.abco@gmail.com>
2019-06-17 01:37:43 -0500

    Pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,15 +17,30 @@ with open(outputpath, 'r', newline='') as outputfile:
     next(outputreader)
 
 #count number of months
-    
+    monthrow = 0
     for row in outputreader:
         date.append(row[0])
         revenue.append(row[1])
-monthrow = len(date)
+        monthrow += 1
 
 #create greatest increase and decrease with corresponding
 #dates for the entries
+greatestinc = revenue[0]
+greatesetdec = revenue[0]
+greatesetincmonth = date[0]
+greatestdecmonth = date[0]
+totalrev = 0
 
+#create loop for the revenue column to determine greatest
+#increase and decrease and determine total revenue
+for i in range(len(revenue)):
+    if revenue[i] >= greatestinc:
+        greatestinc = revenue[i]
+        greatestincmonth = date[i]
+    elif revenue[i] <= greatestdec:
+        greatestdec = revenue[i]
+        greatestdecmonth = date[i]
+        
 
 #print summary
 print("Financial Analysis")
