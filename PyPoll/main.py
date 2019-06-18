518cc912a35ec717dd8149694fbbebc5e6daf4de
Justin Co <justin.abco@gmail.com>
2019-06-17 21:54:36 -0500

    adding PyPoll draft code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,9 +3,12 @@ import csv
 
 electionfile = os.path.join('..', 'PyPoll', 'election_data.csv')
 #dictionary for candidate vote count
-poll = {}
+candidates = [] 
+totalcandidates = [] 
+candidatepercent = [] 
+candidatetotal = []
 
-totalvote = 0
+
 
 
 #open csv file and read commands
@@ -14,22 +17,46 @@ with open(electionfile, 'r', newline = '') as infile:
     #skip header rows 
     next(read)
     
+    totalvote = 0
+    
     for row in read:
+        totalcandidates.append(row[2])
         totalvote += 1
-        if row[2] in poll.keys():
-            poll[row[2]] = poll[row[2]] + 1
-        else:
-            poll[row[2]] = 1
-
-
-#create lists for candidates and vote numbers and percent
-candidate = []
-numvotes = []
-voteper = []
-
-
+        
+sortcandidate = sorted(totalcandidates)
+        
+for h in range(totalvote):
+    if sortcandidate[h-1] != sortcandidate[h]:
+        candidates.append(sortcandidate[h])
+for i in range (len(candidates)):
+    candidatecount = 0 
+      
+    for j in range (len(sortcandidate)):
+        if candidates[i] == sortcandidate[j]:
+            candidatecount += 1
+    candidatepercent.append(round(candidatecount / totalvote * 100, 1))
+    candidatetotal.append(candidatecount) 
 
 print("Election Results")
 print("------------------------")
 print("Total Votes: " + str(totalvote))
-print("------------------------")       
+print("------------------------")  
+
+zipcandidates = zip(candidates, candidatepercent, candidatetotal)  
+for row in zipcandidates:
+    print(row[0] + ":", str(row[1]) + "%", "(" + str(row[2]) + ")")
+
+for k in range(len(candidatepercent)):
+    if candidatetotal[k] > candidatetotal[k-1]:
+        winnercandidate = candidates[k]
+print("------------------------")  
+print("Winner: ", str(winnercandidate))
+print("------------------------")  
+
+        
+#create lists for candidates and vote numbers and percent
+
+#combine candidate, number of votes, and vote percentage in zip
+
+
+  
