44ac04ba3fce8cf1eefa151456442047608f1c19
Justin Co <justin.abco@gmail.com>
2019-06-17 19:06:18 -0500

    --all

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,30 @@
 import os 
 import csv
 
+electionfile = os.path.join('..', 'PyPoll', 'election_data.csv')
+#dictionary for candidate vote count
+poll = {}
+
+totalvote = 0
+
+
+#open csv file and read commands
+with open(electionfile, 'r', newfile = '') as infile:
+    read = csv.reader(infile, delimiter = ',')
+    #skip header rows 
+    next(read)
+    
+    for row in read:
+        totalvote += 1
+        if row[2] in poll.keys():
+            poll[row[2]] = poll[row[2]] + 1
+        else:
+            poll[row[2]] = 1
+
+
+#create lists for candidates and vote numbers and percent
+candidate = []
+numvotes = []
+voteper = []
+
+       
